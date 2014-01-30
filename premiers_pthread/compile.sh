af1260eed7f403fae8d90f0329f3598ebee42e72
Victorette <giovanni.victorette@gmail.com>
2014-01-30 13:29:05 -0500

    optimized code

---
+++ premiers_pthread/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+clang++ -stdlib=libc++ -std=gnu++11 premiers_pthread.cpp -o test.out
--- premiers_pthread/premiers_pthread.cpp
+++ premiers_pthread/premiers_pthread.cpp
@@ -6,119 +6,109 @@
 // Description : Programme qui trouve à l'aide de la passoire d'Ératosthène,
 // tous les nombres premiers inférieurs à un certain seuil
 // spécifié sur la ligne de commande.
-// Attention, ce programme n'est aucunement optimisé!
 //============================================================================
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include <time.h>
-#include <pthread.h>
+#include <cmath>
 #include "Chrono.hpp"
-struct thread_data {
-	int id;
-	int begin;
-	int end;
-};
+#include <pthread.h>
+
+int numThreads, maxLimit, nextbase;
+char *lArrayPrimes;
+long lSquareRoot;
+
+pthread_mutex_t nextbaselock = PTHREAD_MUTEX_INITIALIZER;
 
 void *getPrimeNumber(void *threadarg) {
-	struct thread_data *my_data;
-	my_data = (struct thread_data *) threadarg;
+	int base;
+	do {
+		pthread_mutex_lock(&nextbaselock);
+		base = nextbase;
+		nextbase += 2;
+		pthread_mutex_unlock(&nextbaselock);
 
-	// Allouer le tableau des drapeaux (flags) d'invalidation
-	char *lFlags = (char*) calloc(my_data->end, sizeof(*lFlags));
-	assert(lFlags != 0);
-
-	// Appliquer la passoire d'Ératosthène
-	for (unsigned long p = 2; p < my_data->end; p++) {
-		if (lFlags[p] == 0) {
-			// invalider tous les multiples
-			for (unsigned long i = 2; i * p < my_data->end; i++) {
-				lFlags[i * p]++;
+		if ((int)lArrayPrimes[base] == 0) {
+			for (int i = base; i * base <= maxLimit; i += 2){
+				lArrayPrimes[i * base]++;
 			}
 		}
-	}
-	pthread_exit((void*) lFlags);
+
+	} while(base <= lSquareRoot);
+
+	pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
 
-	unsigned long limiteMax;
 	int numThreads;
-	int searchRangeSize;
-	void* flagedNumbers;
-	int returnCall;
+	int returnCode;
 	char buffer[256];
 
 	if (argc >= 2) {
-		limiteMax = atol(argv[1]);
+		maxLimit = atol(argv[1]);
+		numThreads = atol(argv[2]);
 	} else {
 		printf("Déterminer la limite supérieure pour la recherche: ");
 		fgets(buffer, 256, stdin);
-		limiteMax = atol(buffer);
-		if (limiteMax == 0) {
+		maxLimit = atol(buffer);
+		if (maxLimit == 0) {
 			printf("limite non valide, par défaut, prendre 1000 \n");
-			limiteMax = 1000;
+			maxLimit = 1000;
+		}
+		printf("Déterminer le nombre de fils d'exécution désiré: ");
+		fgets(buffer, 256, stdin);
+		numThreads = atoi(buffer);
+		if (numThreads == 0) {
+			printf("nombre de threads non valide, par défaut, prendre 1 \n");
+			numThreads = 1;
 		}
 	}
 
-	printf("Déterminer le nombre de fils d'exécution désiré: ");
-	fgets(buffer, 256, stdin);
-	numThreads = atoi(buffer);
-	if (numThreads == 0) {
-		printf("nombre de threads non valide, par défaut, prendre 1 \n");
-		numThreads = 1;
+
+	lArrayPrimes = (char *) calloc(maxLimit, sizeof(char *));
+	// Multiples de 2
+	lArrayPrimes[1]++;
+	for (unsigned long i = 4; i <= maxLimit; i += 2) {
+		lArrayPrimes[i]++;
 	}
-
-	searchRangeSize = limiteMax / numThreads;
-
+	nextbase = 3;
+	lSquareRoot = sqrt(maxLimit);
 	pthread_t threads[numThreads];
-	pthread_attr_t attr;
-	struct thread_data thread_data_array[numThreads];
 
 	// Démarrer le chronomètre
 	Chrono lChrono(true);
 
-	/* Initialize and set thread detached attribute */
-	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
-	int startPoint = 0;
 	for (int t = 0; t < numThreads; t++) {
-		thread_data_array[t].id = t;
-		thread_data_array[t].begin = startPoint;
-		thread_data_array[t].end = startPoint + searchRangeSize;
-		startPoint = startPoint + searchRangeSize;
-
-		returnCall = pthread_create(&threads[t], &attr, getPrimeNumber, (void *) &thread_data_array[t]);
-		if (returnCall) {
-			printf("ERROR; return code from pthread_create() is %d\n", returnCall);
+		returnCode = pthread_create(&threads[t], NULL, getPrimeNumber, (void *) &t);
+		if (returnCode) {
+			printf("ERROR; return code from pthread_create() is %d\n", returnCode);
 			exit(-1);
 		}
 	}
-	char *lFlags;
 	for (int t = 0; t < numThreads; t++) {
-		returnCall = pthread_join(threads[t], &flagedNumbers);
-		if (returnCall) {
-			printf("ERROR; return code from pthread_join() is %d\n", returnCall);
+		returnCode = pthread_join(threads[t], NULL);
+		if (returnCode) {
+			printf("ERROR; return code from pthread_join() is %d\n", returnCode);
 			exit(-1);
 		}
-		lFlags = (char *) flagedNumbers;
 	}
-
 	// Arrêter le chronomètre
 	lChrono.pause();
 
 	// Afficher les nombres trouvés à la console
-	for (unsigned long p = 2; p < limiteMax; p++) {
-		if (lFlags[p] == 0)
-			printf("%ld ", p);
+	int count = 0;
+	for (int i = 1; i <= maxLimit; i++) {
+		if ((int) lArrayPrimes[i] == 0) {
+			//printf("%i ", i);
+			count++;
+		}
 	}
 	printf("\n");
-
+	printf("Primes numbers found : %i ", count);
 
 	// Afficher le temps d'exécution dans le stderr
-	fprintf(stderr, "Temps d'execution = %f sec\n", lChrono.get());
-
+	printf("Temps d'execution = %f sec\n", lChrono.get());
 	return 0;
 }
