b592c5bb854d6287e8aad68fb2890bd4c4c0ff5d
Jimmy St-Hilaire <jimmy.sthilaire@gmail.com>
2014-01-31 11:09:33 -0500

    Version multifilaire de la passoire d'Eratosthene.

--- premiers_pthread/Jimmy/trouverPremiers.cc
+++ premiers_pthread/Jimmy/trouverPremiers.cc
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+
+#include <pthread.h>
+
 #include "Chrono.hpp"
+
+char *gArrayPrimes;
+unsigned int gNbPremierASupp;
+unsigned int gMaxLimit;
+long gLimiteSuppSqrt;
+
+pthread_mutex_t gLockIncrement = PTHREAD_MUTEX_INITIALIZER;
 
 void usage (char *iProgram) {
 	std::cout << "Usage> " << iProgram << " limite_prime_number number_of_threads" << std::endl;
@@ -26,49 +36,103 @@ void printPrimes(char* iArray, unsigned int iSize) {
 	std::cout << std::endl;
 }
 
+int countPrimes(char* iArray, unsigned int iSize) {
+	int nbPrime = 0;
+	for (unsigned int i = 1 ; i <= iSize ; i++) {
+		if ((int)iArray[i] == 0) {
+			nbPrime++;
+		}
+	}
+
+	return nbPrime;
+}
+
+void * epurerNombrePremier(void * iParam) {
+	unsigned int currentNumber = 0;
+
+	do {
+		pthread_mutex_lock(&gLockIncrement);
+		currentNumber = gNbPremierASupp;
+		gNbPremierASupp += 2;
+//		std::cout << *lThreadId << " : " << currentNumber << std::endl;
+		pthread_mutex_unlock(&gLockIncrement);
+
+		if (currentNumber > gLimiteSuppSqrt) {
+			return NULL;
+
+		}
+
+		if (gArrayPrimes[currentNumber] == 0) {
+
+			for (unsigned int i = currentNumber * currentNumber ; i <= gMaxLimit ; i += 2 * currentNumber) {
+				gArrayPrimes[i]++;
+
+			}
+		}
+	}
+	while (true);
+}
+
 int main(int argc, char **argv) {
 
 	if (argc < 3 || argc > 3) {
 		usage(argv[0]);
-		std::cout << "Default values of 1000 as the upper limit and 1 for the number of threads will be used." << std::endl;
 	}
 	
-	unsigned int lMaxLimit = atoi(argv[1]);
-	unsigned int lNbThreads = atoi(argv[2]);
+	gMaxLimit = atoi(argv[1]);
+	unsigned short lNbThreads = atoi(argv[2]);
 
-	char *lArrayPrimes = (char *) calloc(lMaxLimit, sizeof(char *));
+	pthread_t lThreads[lNbThreads];
 
-	if (lArrayPrimes == NULL) {
+	gNbPremierASupp = 3;
+	gArrayPrimes = (char *) calloc(gMaxLimit, sizeof(char *));
+
+	if (gArrayPrimes == NULL) {
 		std::cout << "Not enough memory to allocate array space." << std::endl;
 		exit(-2);
 	}
 
-	std::cout << "Travail Pratique 1 : Trouver les " << lMaxLimit << " premiers nombres premiers a l'aide de " << lNbThreads << " threads." << std::endl;
+	std::cout << "Travail Pratique 1 : Trouver les " << gMaxLimit << " premiers nombres premiers a l'aide de " << lNbThreads << " threads." << std::endl;
 
 	Chrono lChrono(true);
 
-	for (unsigned long long i = 4 ; i <= lMaxLimit ; i += 2) {
-		lArrayPrimes[i]++;
+	for (unsigned long long i = 4 ; i <= gMaxLimit ; i += 2) {
+		gArrayPrimes[i]++;
 	}
 
-	long lSquareRoot = sqrt(lMaxLimit);
+	gLimiteSuppSqrt = sqrt(gMaxLimit);
+	
+	for (int i = 0 ; i <= lNbThreads ; i++) {
+		pthread_create(&lThreads[i], NULL, epurerNombrePremier, NULL);
+	}
 
+	for (int i = 0 ; i <= lNbThreads ; i++) {
+		pthread_join(lThreads[i], NULL);
+	}
+
+// 	epurerNombrePremier(&lThreadId);
+
+
+	/*
 	for (unsigned int i = 3 ; i <= lSquareRoot ; i += 2) {
 		if ((int)lArrayPrimes[i] == 0) {
-			for (unsigned int j = i*i ; j <= lMaxLimit ; j += 2 * i) {
+			for (unsigned int j = i*i ; j <= gMaxLimit ; j += 2 * i) {
 				lArrayPrimes[j]++;
+				epurerNombrePremier(NULL);
+				
 			}
 		}
 	}
+	*/
 
 	lChrono.pause();
 
-	printPrimes(lArrayPrimes, lMaxLimit);
-//	printArray(lArrayPrimes, lMaxLimit);
+//	printPrimes(gArrayPrimes, gMaxLimit);
+//	printArray(gArrayPrimes, gMaxLimit);
 
-	std::cout << "Travail effectue en " << lChrono.get() << " secondes." << std::endl;
+	std::cout << countPrimes(gArrayPrimes, gMaxLimit) << " primes found in " << lChrono.get() << " seconds." << std::endl;
 
-	free(lArrayPrimes);
+	free(gArrayPrimes);
 
 	return 0;
 }
