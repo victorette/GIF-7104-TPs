f0a50da13b7489f71e4a95c2f28dc35048aed567
Jimmy St-Hilaire <jimmy.sthilaire@gmail.com>
2014-02-07 15:45:47 -0500

    Version du travail pratique provenant de l'enseignant.

--- openMP/Jimmy/premiers.cpp
+++ openMP/Jimmy/premiers.cpp
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-//#include <omp.h>
-//#include "Chrono.hpp"
-#include <time.h>
+#include "Chrono.hpp"
 
 // Programme qui trouve à l'aide de la passoire d'Ératosthène,
 // tous les nombres premiers inférieurs à un certain seuil 
@@ -13,49 +11,39 @@ int main(int argc, char *argv[])
 {
     // Déterminer la limite supérieure pour la recherche;
     // par défaut, prendre 1000
-    unsigned long lMax = 1000000000;
+    unsigned long lMax = 1000;
     if (argc >= 2) {
         lMax = atol(argv[1]);
     }
 
     // Démarrer le chronomètre
-    //Chrono lChrono(true);
-    clock_t start, stop;
-    start = clock();
+    Chrono lChrono(true);
  
     // Allouer le tableau des drapeaux (flags) d'invalidation
     char *lFlags = (char*) calloc(lMax, sizeof(*lFlags));
     assert(lFlags != 0);
-    unsigned long p;
-    //#pragma omp parallel shared(lFlags) private(p)
-    {
-        // Appliquer la passoire d'Ératosthène
-        #pragma omp for schedule(static)
-        for (p=2; p < lMax; p++) {
-            if (lFlags[p] == 0) {
-                // invalider tous les multiples
-                for (unsigned long i=2; i*p < lMax; i++) {
-                    lFlags[i*p]++;
-                }
+
+    // Appliquer la passoire d'Ératosthène
+    for (unsigned long p=2; p < lMax; p++) {
+        if (lFlags[p] == 0) {
+            // invalider tous les multiples
+            for (unsigned long i=2; i*p < lMax; i++) {
+                lFlags[i*p]++;
             }
         }
     }
+
     // Arrêter le chronomètre
-    //lChrono.pause();
-    stop = clock();
-    double tm = (double) (stop-start)/CLOCKS_PER_SEC;
+    lChrono.pause();
 
     // Afficher les nombres trouvés à la console
-    int count = 0;
     for (unsigned long p=2; p<lMax; p++) {
-        if (lFlags[p] == 0) count++;//printf("%ld ", p);
+        if (lFlags[p] == 0) printf("%ld ", p);
     }
-    printf("Total : %i",count);
     printf("\n");
 
     // Afficher le temps d'exécution dans le stderr
-    //fprintf(stderr, "Temps d'execution = %f sec\n", lChrono.get());
-    printf("Temps d'execution : %lf s\n", tm); 
+    fprintf(stderr, "Temps d'execution = %f sec\n", lChrono.get());
  
     return 0;
 }
